c6e7951f298218b137d072eb44036f75ff078e0f
rubyphunk <treas@dynamicdudes.com>
2008-04-06 23:57:05 +0200

    Moved prototype source into old + Window layout of new

--- old/tst.rb
+++ old/tst.rb
@@ -18,6 +18,15 @@ Curses.program do |scr|
   scr.setpos(scr.maxy - 1, 0)
   scr.addstr("Mode: Run all specs")
   
+  workspace = scr.subwin(5, 5, 10, 10)
+  10.times do |y|
+    workspace.setpos(y, 0)
+    sleep 0.1
+    workspace.addstr("#")
+    workspace.scroll
+    workspace.refresh
+  end
+  
   # 100.times do
   #   scr.setpos(rand(max_y), rand(max_x))
   #   scr.addstr(str)
