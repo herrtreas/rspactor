a52176b6c11ac892e5d99f3746d073fad24fd1de
Andreas Wolff <treas@dynamicdudes.com>
2008-06-04 23:40:33 +0200

    Added (restored from my brain) drawer display and positioning

--- app/controller/DrawerController.rb
+++ app/controller/DrawerController.rb
@@ -9,7 +9,7 @@ class DrawerController < OSX::NSWindowController
   
   def init
     @defaults = NSUserDefaults.standardUserDefaults
-    super_init    
+    super_init
   end
   
   def awakeFromNib
@@ -18,21 +18,27 @@ class DrawerController < OSX::NSWindowController
   
   def setDrawerPosition(sender)
     if sender.title == 'Left'
-      sender.state = 1
-      sender.menu.itemWithTitle('Right').state = 0
       edge = 0 
     else
-      sender.state = 1
-      sender.menu.itemWithTitle('Left').state = 0
       edge = 2
     end
     
     @filesDrawer.openOnEdge(edge)
     @defaults.setObject_forKey(edge.to_s, 'files_drawer_position')  
+    set_current_position_from_defaults
   end
   
   def toggleDrawerVisibility(sender)
-    @filesDrawer.toggle(self)
+    set_current_position_from_defaults
+    if @filesDrawer.state == 0
+      @filesDrawer.openOnEdge(@filesDrawer.preferredEdge)
+    else
+      @filesDrawer.close
+    end
+  end
+  
+  def drawerWillOpen(notification)
+    set_current_position_from_defaults
   end
   
   def drawerDidOpen(notification)
@@ -58,18 +64,30 @@ class DrawerController < OSX::NSWindowController
       drawer_size = size
     else
        drawer_size = @filesDrawer.contentSize
-    end    
+    end 
     
     @filesDrawer.setContentSize(drawer_size)
-    
-    if @defaults.stringForKey("drawer_is_open") == 'true'
-      drawer_position = @defaults.stringForKey("files_drawer_position") || '0'
-      @filesDrawer.openOnEdge(drawer_position)
-      if drawer_position == '0'
-        NSApp.menu.itemWithTag('2').submenu.itemWithTag('22').submenu.itemWithTag('221').state = 1
-      else
-        NSApp.menu.itemWithTag('2').submenu.itemWithTag('22').submenu.itemWithTag('222').state = 1
-      end
-    end
+    set_current_position_from_defaults    
+    drawer_open_by_default  
   end
+  
+  def set_current_position_from_defaults
+    drawer_position = @defaults.stringForKey("files_drawer_position")
+    drawer_position = '0' if drawer_position.nil?    
+    if drawer_position == '0'
+      NSApp.menu.itemWithTag('2').submenu.itemWithTag('22').submenu.itemWithTag('221').state = 1
+      NSApp.menu.itemWithTag('2').submenu.itemWithTag('22').submenu.itemWithTag('222').state = 0      
+    else
+      NSApp.menu.itemWithTag('2').submenu.itemWithTag('22').submenu.itemWithTag('222').state = 1
+      NSApp.menu.itemWithTag('2').submenu.itemWithTag('22').submenu.itemWithTag('221').state = 0      
+    end      
+    @filesDrawer.preferredEdge = drawer_position.to_i      
+  end
+    
+  def drawer_open_by_default
+    drawer_open = @defaults.stringForKey("drawer_is_open")
+    drawer_open = 'true' if drawer_open.nil?    
+    @filesDrawer.openOnEdge(@filesDrawer.preferredEdge) if drawer_open == 'true'
+  end
+  
 end
