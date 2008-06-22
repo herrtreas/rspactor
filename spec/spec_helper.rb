3320687fa2bfe8e114176e3cc5703fa67437a906
rubyphunk <treas@dynamicdudes.com>
2008-06-22 18:38:24 +0200

    Removin old stuff

--- spec/spec_helper.rb
+++
@@ -1,5 +0,0 @@
-$:.unshift File.join(File.dirname(__FILE__), '../app')
-$fpath_simple   = File.join(File.dirname(__FILE__), 'fixtures/maps/simple')
-$fpath_doubles  = File.join(File.dirname(__FILE__), 'fixtures/maps/doubles')
-
-require File.join(File.dirname(__FILE__), '../rb_main')
