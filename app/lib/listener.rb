2db5de3a12a7a35c797cb2443911dfcd1732c9fb
rubyphunk <treas@dynamicdudes.com>
2008-07-07 11:16:10 +0200

    Removed super verbose logger output

--- app/lib/listener.rb
+++ app/lib/listener.rb
@@ -3,10 +3,8 @@ class Listener
   
   @@callback = Proc.new do |stream, ctx, num_events, paths, marks, event_ids|
     begin
-      $LOG.debug "X listener.rb:6"
       changed_files = extract_changed_files_from_paths(split_paths(paths, num_events))        
       @@spec_run_time = Time.now
-      $LOG.debug "X listener.rb:9"      
       @@block_to_execute.call(changed_files)
     rescue => e
       $LOG.error "#{e.message}: #{e.backtrace.first}"
@@ -18,7 +16,6 @@ class Listener
     Map.ensure(path) do
       @@listener = Listener.new(path) do |files|        
         begin
-          $LOG.debug "X listener.rb:21. Files#{@files.inspect}"          
           @files_to_spec = []
           files.each do |file|
             spec_file = $map[file]
@@ -27,7 +24,6 @@ class Listener
               @files_to_spec << spec_file 
             end
           end 
-          $LOG.debug "X listener.rb:30: Files#{@files_to_spec.inspect}"          
           SpecRunner.run_specs_for_files(@files_to_spec) unless @files_to_spec.empty?
         rescue => e
           $LOG.error "#{e.message}: #{e.backtrace.first}"
@@ -43,12 +39,9 @@ class Listener
   
   def initialize(path, &block)    
     begin     
-      $LOG.debug "X listener.rb:46"          
-       
       @@block_to_execute = block
       @@spec_run_time = Time.now
       OSX.require_framework '/System/Library/Frameworks/CoreServices.framework/Frameworks/CarbonCore.framework'
-      @stream = OSX::FSEventStreamCreate(OSX::KCFAllocatorDefault, @@callback, nil, [path], OSX::KFSEventStreamEventIdSinceNow, 0.0, 0)
       
       $LOG.debug "Listening to '#{path}'.."
       OSX::FSEventStreamScheduleWithRunLoop(@stream, OSX::CFRunLoopGetMain(), OSX::KCFRunLoopDefaultMode)
@@ -72,17 +65,14 @@ class Listener
   end
   
   def self.split_paths(paths, num_events)
-    $LOG.debug "X listener.rb:73"              
     paths.regard_as('*')
     rpaths = []        
     num_events.times { |i| rpaths << paths[i] }
-    $LOG.debug "X listener.rb:77"              
     rpaths    
   end
   
   def self.extract_changed_files_from_paths(paths)
     begin
-      $LOG.debug "X listener.rb:83"                
       changed_files = []
       paths.each do |path|
         Dir.glob(path + "*").each do |file|
@@ -91,7 +81,6 @@ class Listener
           changed_files << file if file_time > @@spec_run_time
         end
       end
-      $LOG.debug "X listener.rb:92"                
       changed_files
     rescue => e
       $LOG.error "#{e.message}: #{e.backtrace.first}"
--- app/lib/spec_runner.rb
+++ app/lib/spec_runner.rb
@@ -1,26 +1,19 @@
 module SpecRunner
   class << self
     def run_in_path(path)
-      $LOG.debug "X spec_runner.rb:4"          
-      
       Map.ensure(path) do
-        $LOG.debug "X spec_runner.rb:7"
         run_all_specs_in_path
       end
     end
     
     def run_all_specs_in_path
       return false if $map.spec_files.empty?  # TODO: Notify user
-      $LOG.debug "X spec_runner.rb:15"
       run_command(create_runner_arguments($map.spec_files))
     end
     
     def run_specs_for_files(files)
-      $LOG.debug "X spec_runner.rb:21"      
       return false if files.empty?            # TODO: Notify user
-      $LOG.debug "X spec_runner.rb:23: Command running: #{command_running?}"            
       return false if command_running?      
-      $LOG.debug "X spec_runner.rb:25"      
       $app.post_notification :spec_run_invoked
       run_command(create_runner_arguments(files))
     end
